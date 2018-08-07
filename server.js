bf5cebd1e50863ad0039d2e1f6ea2e2bb96d459c
joseachig28 <jose.achig.u@gmail.com>
2018-08-07 08:46:21 -0500

    updates to deploy heroku

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = express();
 const path=require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/mongo'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/mongo/index.html'));
   });
 
   console.log('Console listening!');
